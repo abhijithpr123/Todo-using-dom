9170e180cd92b5a9830f2241e859a83941c6d37f
abhijithpr123 <abhipr222@gmail.com>
2025-10-26 16:40:29 +0530

    add, delete, display funtions completed

---
+++ js/index.js
@@ -0,0 +1,32 @@
+let Todo=[];
+
+document.getElementById("add").addEventListener('click',()=>{
+    let task = document.getElementById("task").value;
+    if(task=="")
+        return alert("Enter your task name")
+    Todo.push(task)
+    document.getElementById("task").value="";
+    display()
+})
+
+function display(){
+    let str =``;
+    for(const[index,task] of Todo.entries()){
+        str+= `
+        <tr>
+            <td>${index+1}</td>
+            <td>${task}</td>
+            <td><button id="edit">Edit</button></td>
+            <td><button id="delete" onclick="deleteTask(${index})">Delete</button></td>
+        </tr>
+        ` 
+    }
+    document.getElementById("display").innerHTML=str
+}
+function deleteTask(index){
+    let task = Todo[index];
+    if(confirm(`Are you sure "${task}" want to delete?`)){
+        Todo.splice(index,1);
+        display()
+    }
+}
